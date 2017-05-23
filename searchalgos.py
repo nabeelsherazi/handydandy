0a72ccc9de98bbe7dc05a574b6b2277ab74dc33b
nabeelsherazi <nabeelsherazi97@gmail.com>
2017-05-23 06:51:03 -0400

    PEP8 compliance

--- searchalgos.py
+++ searchalgos.py
@@ -1,13 +1,16 @@
-import sys, time
-sys.path.append('C:\\Users\\nxtfa\\Desktop\\Programming\\my lib')
+import sys
+import time
 from testsuite import *
+sys.path.append('C:\\Users\\nxtfa\\Desktop\\Programming\\my lib')
+
 
 def search_linear(xs, target):
     """ Find and return the index of target in sequence xs """
     for (i, v) in enumerate(xs):
-       if v == target:
-           return i
+        if v == target:
+            return i
     return -1
+
 
 def find_unknown_words(vocab, wds):
     '''return a list of words in wds that do not occur in vocab'''
@@ -17,6 +20,7 @@ def find_unknown_words(vocab, wds):
             result.append(w)
     return result
 
+
 def load_words_from_file(filename):
     """ Read words from filename, return list of words. """
     f = open(filename, "r")
@@ -24,6 +28,7 @@ def load_words_from_file(filename):
     f.close()
     wds = file_content.split()
     return wds
+
 
 def text_to_words(the_text):
     """return a list of words with all punctuation removed, and all in lowercase."""
@@ -39,6 +44,7 @@ def text_to_words(the_text):
     wds = cleaned_text.split()
     return wds
 
+
 def get_words_in_book(filename):
     """ Read a book from filename, and return a list of its words. """
     f = open(filename, "r")
@@ -47,13 +53,14 @@ def get_words_in_book(filename):
     wds = text_to_words(content)
     return wds
 
+
 def search_binary(xs, target, verbose=False):
     """ Find and return the index of key in sequence xs """
     lb = 0
     ub = len(xs)
     while True:
         if lb == ub:   # If region of interest (ROI) becomes empty
-           return -1
+            return -1
 
         # Next probe should be in the middle of the ROI
         mid_index = (lb + ub) // 2
@@ -62,7 +69,7 @@ def search_binary(xs, target, verbose=False):
         item_at_mid = xs[mid_index]
         if verbose:
             print("ROI[{0}:{1}](size={2}), probed='{3}', target='{4}'"
-               .format(lb, ub, ub-lb, item_at_mid, target))
+                  .format(lb, ub, ub-lb, item_at_mid, target))
 
         # How does the probed item compare to the target?
         if item_at_mid == target:
@@ -71,6 +78,7 @@ def search_binary(xs, target, verbose=False):
             lb = mid_index + 1    # Use upper half of ROI next time
         else:
             ub = mid_index        # Use lower half of ROI next time
+
 
 if __name__ == "__main__":
     # Linear algorithm test suite
@@ -82,7 +90,7 @@ if __name__ == "__main__":
 
     # Find unknown words in list test suite
     vocab = ["apple", "boy", "dog", "down",
-                              "fell", "girl", "grass", "the", "tree"]
+             "fell", "girl", "grass", "the", "tree"]
     book_words = "the apple fell from the tree to the grass".split()
     test(find_unknown_words(vocab, book_words) == ["from", "to"])
     test(find_unknown_words([], book_words) == book_words)
@@ -91,10 +99,10 @@ if __name__ == "__main__":
     # Text to words test suite
     test(text_to_words("My name is Earl!") == ["my", "name", "is", "earl"])
     test(text_to_words('"Well, I never!", said Alice.') ==
-                                 ["well", "i", "never", "said", "alice"])
+         ["well", "i", "never", "said", "alice"])
 
     # Binary algorithm test suite
-    xs = [2,3,5,7,11,13,17,23,29,31,37,43,47,53]
+    xs = [2, 3, 5, 7, 11, 13, 17, 23, 29, 31, 37, 43, 47, 53]
     test(search_binary(xs, 20) == -1)
     test(search_binary(xs, 99) == -1)
     test(search_binary(xs, 1) == -1)
